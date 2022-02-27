872aac631a92fa3a78438257ecff8082d04449b0
pastelite <kana.bhong@gmail.com>
2022-02-27 17:14:56 +0700

    Updates

---
+++ _app/chunks/bottombar.svelte_svelte_type_style_lang-54d07e23.js
@@ -0,0 +1,1 @@
+import{D as e}from"./vendor-a6dcf9fe.js";let a=e([{page:"Home",link:"./",onthis:!0},{page:"About me",link:"./about",onthis:!1}]);export{a as c};
---
+++ _app/chunks/utils-e46d1090.js
@@ -0,0 +1,1 @@
+import{c as r}from"./bottombar.svelte_svelte_type_style_lang-54d07e23.js";function a(n){r.update(t=>{for(let e=0;e<t.length;e++)t[e].onthis=!1;return t[n].onthis=!0,t})}export{a as s};
